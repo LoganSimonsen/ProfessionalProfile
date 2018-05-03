5a0f8c7ce5bdec1073606ee977c62ffddabb807f
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-05-03 11:02:57 -0500

    added headshot to resume

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import routes from "./routes";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import image1 from "./screenCapPDF.png";
+import headShot from "./headShot.jpg";
 
 let count = 0;
 class App extends Component {

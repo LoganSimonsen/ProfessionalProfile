2310d01619e483147fcde8fba0818a09281232ac
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-05-10 14:31:59 -0500

    style changes

--- src/components/Accomplishments/Accomplishments.js
+++ src/components/Accomplishments/Accomplishments.js
@@ -9,13 +9,18 @@ class Accomplishments extends Component {
   render() {
     return (
       <div className="projects">
-        <Link to="/" className="backButton">
+        <Link style={{textDecoration: "none"}} to="/" className="backButton">
           X
         </Link>
         <div className="projectsWrapper">
           <div className="singleProjectBox">
             <a className="projectLinks" href="https://contentum.now.sh/#/">
               <h1>Contentum</h1>
+              <p style={{ color: "white", maxWidth: "450px" }}>
+                A content management application where users can store their
+                text, pictures and video for easy access on their external
+                website.
+              </p>
               <br />
               <img src={Contentum} />
             </a>
@@ -36,6 +41,10 @@ class Accomplishments extends Component {
               href="https://openlibrarysearch.now.sh/#/"
             >
               <h1>Open Library Search</h1>
+              <p style={{ color: "white", maxWidth: "450px" }}>
+                Search through thousands of online books that are free to read
+                online!
+              </p>
               <br />
               <img src={OpenLibrarySearch} />
             </a>
@@ -51,9 +60,14 @@ class Accomplishments extends Component {
           <div className="singleProjectBox">
             <a className="projectLinks" href="https://dashyboards.now.sh/#/">
               <h1>Dashy Boards</h1>
+              <p style={{ color: "white", maxWidth: "450px" }}>
+                A Monitoring and Incident management tool designed with Incident
+                Management teams in mind.
+              </p>
               <br />
               <img src={DashyBoards} height="450px" width="450px" />
             </a>
+
             <ul>
               <lh>Technologies Used Include:</lh>
               <li>React</li>
